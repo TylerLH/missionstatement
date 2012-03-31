369c6431305bcfea49c50f43f5413c15ee095d49
Tyler Hughes <iampbt@gmail.com>
2012-03-31 01:46:12 -0400

    added random url string gen

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 Mission Statement
 =============================
+
+Save your ideas in long or short form and stop repeating yourself when you want to explain them.
+------------------------------------------
+
+Runs on Python 2.7.2+, Flask framework, and MongoKit
--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 app = Flask(__name__)
 
 from mongokit import *
-import datetime
+import datetime, string, random
 
 connection = Connection()
 
@@ -11,12 +11,24 @@ connection = Connection()
 class Project(Document):
 	structure = {
 		'title':unicode,
+		'identifier':unicode,
 		'short_desc':unicode,
 		'twitter_desc':unicode,
 		'created_at':datetime.datetime
 	}
+
+	# creates a url string for the project
+	def create_identifier(size=6, chars=string.ascii_uppercase + string.digits):
+		return ''.join(random.choice(chars) for x in range(size))
+
 	required_fields = ['title', 'created_at']
-	default_values = {'created_at':datetime.datetime.utcnow}
+	default_values = {'created_at':datetime.datetime.utcnow, 'identifier':create_identifier()}
+	indexes = [ 
+		{
+			'fields':['identifier'],
+			'unique':True
+		} 
+	]
 
 @app.route("/")
 def hello():
