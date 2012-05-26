f82c96c2506a3ba27b20ed01d52b9608a25be59d
Ilya Bagrak <ilya.bagrak@gmail.com>
2012-05-26 01:51:40 -0400

    Implemented autosave function.
    Lots of little things related to autosave.

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import os
 from datetime import timedelta
 
+try:
+    import simplejson as json
+except ImportError:
+    import json
+    
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash
+     render_template, flash, jsonify, make_response
 app = Flask(__name__)
 
 from flask.ext.mongokit import MongoKit, Document
@@ -42,7 +47,6 @@ app.config.update(
 
 mail = Mail(app)
 
-
 # creates a url string for the project
 def create_url(size=6, chars=string.ascii_uppercase + string.digits):
   return u''.join(random.choice(chars) for x in range(size))
@@ -65,7 +69,7 @@ class Project(Document):
                  }              
 
     required_fields = ['title', 'tagline', 'created_at', 'unique_url']
-    default_values = {'created_at':datetime.datetime.utcnow, 'title':u''}
+    default_values = {'created_at':datetime.datetime.utcnow, 'title':u'*', 'tagline':u''}
     use_dot_notation = True
     indexes = [ 
                {
@@ -127,97 +131,93 @@ class ProjectForm(Form):
     email_addr = TextField("", [email_or_empty()])
 
 @app.route("/", methods=["GET", "POST"])
-def hello():
-    form = ProjectForm(request.form)
+def home():
+    if not 'pitches' in session:
+        session['pitches'] = []
+        session.permanent = True
+            
     project = db.Project()
+    project.unique_url = create_url()
+    project.save()
     
-    if request.method == "POST": 
-        if form.validate():
-            form.populate_obj(project)
-            project.unique_url = create_url()
-            project.save()
-        
-            if not 'pitches' in session:
-                session['pitches'] = []
-                session.permanent = True
-            
-            session['pitches'].append({'name' : project.title, 'url' : project.unique_url})
-            session.modified = True
-        
-            flash('Saved!', category = 'success')
-            app.logger.debug('Updating...')
-        
-            return redirect(url_for('show_project', unique_url=project.unique_url))
-        else:
-            flash(get_errors(form), category = 'error')
-            
-    form = ProjectForm(obj=project)
-    return render_template('new_project.html', form=form, update = False)
+    session['pitches'].append({'name' : project.title, 'url' : project.unique_url})
+    session.modified = True
+    
+    return redirect(url_for('show_project', unique_url = project.unique_url))
+
+@app.route('/favicon.ico')
+def favicon():
+    return abort(404) 
+              
 
 @app.route('/<unique_url>', methods=["GET", "POST"])
 def show_project(unique_url):
     form = ProjectForm(request.form)
-    project = db.Project.find_one({'unique_url':unique_url})
+    project = db.Project.find_one({'unique_url' : unique_url})
     
     # POST
-    if request.method == "POST":
-        if "_update" in request.form.values():
-            if form.validate():
-                form.populate_obj(project)
-                project.save()
+    if request.method == "POST" and "_email" in request.form.values():
+        if not form.email_addr.data: 
+            flash('Email missing', category='error')
                 
-                # session expired (?)
-                if not 'pitches' in session:
-                    session['pitches'] = [{'name' : project.title, 'url' : project.unique_url}]
-                    session.permanent = True
-                        
-                # update cookie if name changed
-                for pitch in session['pitches']:
-                    if pitch['url'] == unique_url:
-                        pitch['name'] = project.title
+        elif mail: 
+            msg = Message(form.title.data + " on Mission Statement",
+                          sender="ilya.bagrak@gmail.com",
+                          recipients=[form.email_addr.data])
+            msg.body = """Hello, %s! 
                     
-                session.modified = True
-                            
-                flash('Updated!', category = 'success')
-                app.logger.debug('Updating...')
-                return redirect(url_for('show_project', unique_url=unique_url))         
-            else: 
-                flash(get_errors(form), category = 'error')
-        
-        elif "_email" in request.form.values():
-            if not form.email_addr.data: 
-                flash('Email missing', category = 'error')
+                          Here is the permanent link to your \"%s\" pitch on Mission Statement (http://missionstatement.herokuapp.com). 
+                          
+                          %s
+                          
+                          Flex that idea muscle and keep them coming!
+                          
+                          Yours Truly, 
+                          
+                          Ilya and Tyler
+                          
+                          PS: Let us know how we can keep Mission Statement better, or better yet send us a pitch.
+                        """ % (form.email_addr.data, form.title.data, "http://missionstatement.herokuapp.com/" + unique_url)
+            mail.send(msg)
                 
-            elif mail: 
-                msg = Message("",
-                              sender="ilya.bagrak@gmail.com",
-                              recipients=[form.email_addr.data])
-                msg.body = """Hello, %s! 
-                    
-                              Here is the link to your \"%s\" pitch on Mission Statement (http://missionstatement.herokuapp.com). 
-                          
-                              %s
-                          
-                              Flex that idea muscle and keep them coming!
-                          
-                              Yours Truly, 
-                          
-                              Ilya and Tyler
-                          
-                              PS: Let us know how we can keep Mission Statement better, or better yet send us a pitch.
-                              """ % (form.email_addr.data, form.title.data, "http://missionstatement.herokuapp.com/" + unique_url)
-                mail.send(msg)
-                
-                app.logger.debug('Emailing...')
-                flash('Link sent.', category = 'success')
-            else:
-                flash(get_errors(form), category = 'error')
+            app.logger.debug('Emailing...')
+            flash('Link sent.', category='success')
+        else:
+            flash(get_errors(form), category='error')
                     
         return redirect(url_for('show_project', unique_url=unique_url))
     
     # GET
     form = ProjectForm(obj=project)
-    return render_template('new_project.html', form=form, update = True)
+    app.logger.debug(project)
+    return render_template('new_project.html', form=form, unique_url = project.unique_url, update = True)
+
+@app.route('/api/v1/pitch/<unique_url>', methods=["POST"])
+def update_project(unique_url):
+    app.logger.debug(request.json)
+    
+    project = db.Project.find_one({'unique_url' : unique_url})
+    if not project: 
+        response = jsonify({'code': 404,'message': 'No pitch found.'})
+        response.status_code = 404
+        return response
+    
+    payload = json.loads(request.data)
+    for k in payload.keys(): 
+        if k in Project.structure: 
+            project[k] = payload[k]
+            if k == 'title': 
+                for pitch in session['pitches']:
+                    if pitch['url'] == unique_url:
+                        pitch['name'] = project.title
+                session.modified = True
+        else: 
+            response = jsonify({'code': 4,'message': 'Unknown field.'})
+            response.status_code = 404
+            return response
+            
+    project.save()
+    return jsonify(code = 2, message = "Success")
 
 if __name__ == "__main__":
     # Bind to PORT if defined, otherwise default to 5000.
--- static/javascripts/missionstatement.js
+++ static/javascripts/missionstatement.js
@@ -1,4 +1,13 @@
+const PARTIALLY_SAVED = 1;   
+const SAVED = 2;             
+const NOT_SAVED = 3
+const UNKNOWN_FIELD = 4
 
+
+//401 Unauthorized - No valid API key provided.
+//402 Request Failed - Parameters were valid but request failed.
+//404 Not Found - The requested item doesn't exist.
+//500, 502, 503, 504 Server errors - something went wrong on Stripe's end.
 
 $(window).load(function() {
     var x = $('.navbar-fixed-top-plus').offset().top + $('.navbar-fixed-top-plus').outerHeight();
@@ -12,8 +21,110 @@ $(document).ready( function() {
     
     $('.alert').delay(3000).fadeOut('slow');
     
+    setInterval(function() {
+        var kvs = {};
+        var size = 0;
+        
+        $.each($('.word_limited, .char_limited'), function(index, value) {
+            if ($(this).hasClass('input-changed') && !$(this).hasClass('input-overflown')) {
+                kvs[$(this).attr('id')] = $(this).val();
+                size++;
+            }
+        });
+        
+        if (size == 0) return;
+        
+        $('#form_status').html('Saving...');
+        var kvs = JSON.stringify(kvs);
+        
+        $.ajax({
+            type: 'POST',
+            url: "/api/v1/pitch" + window.location.pathname,
+            data: kvs,
+            dataType: "json",
+            contentType: "application/json; charset=utf-8"
+        }).done(function(data, code, jqxhr) {
+            var code = data['code'];
+            var message = data['message'];
+            var feedback; 
+            
+            if (code == PARTIALLY_SAVED || code == SAVED) {
+                feedback = 'All changes saved<span id="saved_ago"></span>.'
+            } else {
+                feedback = 'An unknown error has occured.'
+            }
+            
+            if ($('.input-overflown').length > 0) {
+                feedback = feedback + ' Be sure to correct other fields.';
+            }
+            
+            $('.word_limited, .char_limited').removeClass('input-changed');
+            setTimeout(function() {$('#form_status').html(feedback);}, 400);
+            
+        }).fail(function(jqxhr, code, exception) {
+            // TODO: Error handling
+            $('#form_status').html('A ' + jqxhr.status + ' error occured while saving.');
+        });
+        
+    }, 5000);
+    
+    $('#title').keyup(function() {
+        var title = $(this).val().substring(0,17);
+        
+        
+        if($(this).val().length >= 17) {
+            $('button[class*="disabled"]').html(title + '...');
+        } else {
+            $('button[class*="disabled"]').html(title);
+        }
+        
+        // move down the body if we have to
+        var x = $('.navbar-fixed-top-plus').offset().top + $('.navbar-fixed-top-plus').outerHeight();
+        $('body').css('padding-top', x + 10 + 'px');
+    });
+    
     $('.word_limited, .char_limited').keyup(function() {
         
+        if ($(this).parents('arclones').length > 0) {
+            return;
+        }
+        
+        var contents = $(this).val();
+        var count_words = $(this).hasClass('word_limited');
+        var limit = $(this).attr('size');
+        var help_count = $(this).parent().find('.units-left');
+        var help_plural = $(this).parent().find('.plural');
+        var help_warning = $(this).parent().find('.too-much');
+        var help = $(this).parent().find('.help-inline');
+        
+        if (count_words) {
+            var count = contents == "" ? 0 : contents.trim().split(' ').length
+        } else {
+            var count = contents.length
+        }
+        
+        if (Math.abs(count - limit) == 1) {
+            help_plural.html('');
+        } else {
+            help_plural.html('s');
+        }
+        
+        if (count > limit) {
+            help_warning.html('too many');
+            help.addClass('help-overflown');
+            $(this).addClass('input-overflown');
+        } else {
+            help_warning.html('left');
+            help.removeClass('help-overflown');
+            $(this).removeClass('input-overflown');
+            $(this).addClass('input-changed');
+        }
+        
+        help_count.html(Math.abs(limit - count));
+    });
+    
+    //initial run through
+    $.each($('.word_limited, .char_limited'), function() {
         if ($(this).parents('arclones').length > 0) {
             return;
         }
@@ -50,7 +161,5 @@ $(document).ready( function() {
         
         help_count.html(Math.abs(limit - count));
     });
-    
-    $('.word_limited, .char_limited').trigger('keyup');
 });
-  
+ 
